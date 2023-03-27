009e8cd0d63ec390c3e6b2bcde0a629fac04084c
Airenas Vaičiūnas <airenass@gmail.com>
2023-03-27 09:48:10 +0300

    Take fake from body

--- deploy/az_f/bankmap/function_app.py
+++ deploy/az_f/bankmap/function_app.py
@@ -41,30 +41,25 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
     logger.info("version {}".format(app_ver))
     start, metrics = time.time(), {}
     logger.info("got request")
-    file = req.files.get("file", None)
-    if not file:
-        return json_resp({"error": "no file"}, HTTPStatus.BAD_REQUEST)
-    company = req.form.get("company", None)
-    if not company:
-        return json_resp({"error": "no company"}, HTTPStatus.BAD_REQUEST)
-    extract_dir = req.form.get("extract_dir", "data")
+
+    company = req.headers.get("company", None)
     logger.info("company {}".format(company))
-    logger.info("file {}".format(file.name))
     try:
+        zip_bytes = req.get_body()
         temp_dir = tempfile.TemporaryDirectory()
-        logger.info("tmp dir {}".format(temp_dir))
+        logger.info("tmp dir {}".format(temp_dir.name))
         out_file = os.path.join(temp_dir.name, "in.zip")
         logger.info("out_file {}".format(out_file))
         with open(out_file, "wb") as f:
-            file.save(f)
-        logger.info("saved file {}".format(out_file))
+            f.write(zip_bytes)
+        logger.info("saved file {} ({}b)".format(out_file, len(zip_bytes)))
         next_t = log_elapsed(start, "save_zip", metrics)
 
+        data_dir = os.path.join(temp_dir.name, "data")
         with zipfile.ZipFile(out_file) as z:
-            z.extractall(temp_dir.name)
-        logger.info("saved files {}".format(temp_dir.name))
+            z.extractall(data_dir)
+        logger.info("saved files to {}".format(data_dir))
         next_t = log_elapsed(next_t, "extract_zip", metrics)
-        data_dir = os.path.join(temp_dir.name, extract_dir)
         logger.info("start mapping")
 
         mappings, info = do_mapping(data_dir, PredictionCfg(company=company, top_best=3))
