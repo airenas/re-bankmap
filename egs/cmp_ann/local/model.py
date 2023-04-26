3927d05062802583467b3314e2ce6777c442a721
Airenas Vaičiūnas <airenass@gmail.com>
2023-04-26 13:56:06 +0300

    Inc training epochs

--- egs/cmp_ann/local/model.py
+++ egs/cmp_ann/local/model.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 def create_model(inp):
     l_input = tf.keras.layers.Input(shape=inp)
     output = l_input
-    # output = tf.keras.layers.Dense(200, activation="relu")(output)
-    # output = tf.keras.layers.Dropout(0.2)(output)
     output = tf.keras.layers.Dense(100, activation="relu")(output)
     output = tf.keras.layers.Dropout(0.2)(output)
     output = tf.keras.layers.Dense(1, activation="sigmoid")(output)
--- egs/cmp_ann/local/similarity.py
+++ egs/cmp_ann/local/similarity.py
@@ -11,10 +11,11 @@ def similarity(ctx: Ctx, ledger: LEntry, entry: Entry, prev_entries):
     res.append(1 if ledger.type == LType.CUST else 0)
 
     # res.append(1 if entry.bank_account == "MASTERCARD" else 0)
-    # res.append(1 if ledger.type == LType.BA else 0)
-    # res.append(1 if ledger.type == LType.VEND else 0)
-    # res.append(1 if ledger.type == LType.CUST else 0)
-    # res.append(1 if ledger.type == LType.GL else 0)
+    # res.append(1 if entry.bank_account == "SEBEUR" else 0)
+    # res.append(1 if entry.bank_account == "VISA" else 0)
+    # res.append(1 if entry.bank_account == "SEBNOK" else 0)
+    # res.append(1 if entry.bank_account == "SEBUSD" else 0)
+
     return res
 
 
