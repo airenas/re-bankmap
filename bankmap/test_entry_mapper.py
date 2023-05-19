37eab8c44135ffb2df5c800961eb6c7f5071413f
Airenas Vaičiūnas <airenass@gmail.com>
2023-05-19 11:29:35 +0300

    Add test

--- bankmap/test_entry_mapper.py
+++ bankmap/test_entry_mapper.py
@@ -16,6 +16,9 @@ def test_get_confidence():
     assert get_confidence(0.8, "Customer",
                           PredictionCfg(limit=1.5, limits={"Customer": {"1": 1.6, "0.995": 1.5, "0.95": 1.0},
                                                            "all": {"1": 1.5, "0.995": 1.4, "0.95": 0.9}})) == 0.5
+    assert get_confidence(1.512, "Customer",
+                          PredictionCfg(limit=1.5, limits={"Customer": {},
+                                                           "all": {"1": 1.6, "0.995": 1.55, "0.9": 0.9}})) == 0.9
 
 
 def test_calc_confidence():
