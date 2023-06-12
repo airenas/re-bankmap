7fc8ca455bdbbfa797c7b0819c370eb62f46e060
Airenas Vaičiūnas <airenass@gmail.com>
2023-06-12 13:53:39 +0300

    Parse time from logs also

--- egs/logs/local/map_logs.py
+++ egs/logs/local/map_logs.py
@@ -15,7 +15,9 @@ class logData:
         self.lines = strs[3]
         self.recommended = strs[4]
         self.trained = int(strs[6]) if len(strs) > 6 and strs[6] != "" else 0
+        self.trained_date = to_date(strs[7]).strftime("%Y%m%d") if len(strs) > 7 and strs[7] != "" else ""
         self.date = to_date(data.get("timestamp [UTC]", "no date")).strftime("%Y%m%d")
+        self.time = to_date(data.get("timestamp [UTC]", "no date")).strftime("%H:%M:%S")
 
     def to_s(self):
         return "{}\t{}\t{}\t{}\t{}\t{}".format(self.date, self.company, self.trained, self.entries, self.lines,
@@ -36,9 +38,10 @@ def main(argv):
     logger.info("Read {}".format(len(data)))
     for d in data:
         ld = logData(d)
-        res.append([ld.date, ld.company, ld.entries, ld.lines, ld.recommended, ld.trained])
+        res.append([ld.date, ld.company, ld.entries, ld.lines, ld.recommended, ld.trained, ld.trained_date, ld.time])
 
-    df = pd.DataFrame(res, columns=["date", "company", "entries", "lines", "recommended", "trained"])
+    df = pd.DataFrame(res,
+                      columns=["date", "company", "entries", "lines", "recommended", "trained", "trained_date", "time"])
 
     df.to_parquet(args.output, engine="fastparquet")
     logger.info("Done")
