9f136154297b416eb3a1273bba8b0047fc459341
Airenas Vaiciunas <airenass@gmail.com>
2023-10-20 11:44:41 +0300

    Add tune function with azure ML

---
+++ deploy/az_f/bankmap-tune-ml/function_app.py
@@ -0,0 +1,110 @@
+import json
+import os
+from http import HTTPStatus
+
+import azure.functions as func
+from azure.ai.ml import MLClient
+from azure.ai.ml import dsl, Input, Output
+from azure.identity import DefaultAzureCredential
+
+from bankmap.logger import logger
+
+class FynctionCfg:
+     def __init__(self):
+        self.compute_cluster = os.getenv('COMPUTE_CLUSTER', 'cpu-cluster-lp')
+        self.output_path = os.getenv('OUTPUT_DATA_PATH', 'azureml://subscriptions/ae0eff97-7885-4c1e-b23c-d8a627ef292f/resourcegroups/DocuBank/workspaces/test/datastores/configs/paths/')
+        self.input_path_template = os.getenv('INPUT_DATA_PATH_TEMPLATE', 'azureml://subscriptions/ae0eff97-7885-4c1e-b23c-d8a627ef292f/resourcegroups/DocuBank/workspaces/test/datastores/devcopy/paths/{}.zip')
+        self.subscription_id = os.getenv('SUBSCRIPTION_ID')
+        self.workspace = os.getenv('ML_WORKSPACE', "test")
+        self.ml_component = os.getenv('ML_COMPONENT',"bankmap_tune")
+        
+
+app = func.FunctionApp()
+container = "data-copy"
+
+def json_resp(value, code: int):
+    return func.HttpResponse(body=json.dumps(value, ensure_ascii=False), status_code=int(code),
+                             mimetype="application/json")
+
+
+def get_version():
+    try:
+        with open(".version", "r") as f:
+            return f.read().strip()
+    except BaseException as err:
+        logger.error(err)
+        return "???"
+
+
+@app.function_name(name="HTTPTrigger")
+@app.route(route="tune/{blobName}")
+def http_start(req: func.HttpRequest):
+    blob_name = req.route_params.get('blobName')
+    logger.info(f"Python blob trigger function processed blob {blob_name}")
+    try:
+        p_name = process(container + "/" + blob_name)
+        res = {"job_name": p_name}
+        return func.HttpResponse(body=json.dumps(res, ensure_ascii=False), status_code=int(HTTPStatus.OK),
+                                 mimetype="application/json")
+    except BaseException as err:
+        logger.exception(err)
+        return func.HttpResponse(body=json.dumps(str(err), ensure_ascii=False),
+                                 status_code=int(HTTPStatus.INTERNAL_SERVER_ERROROK),
+                                 mimetype="application/json")
+
+
+@app.function_name(name="BlobTrigger")
+@app.blob_trigger(arg_name="zipfile",
+                  path=container + "/{name}.zip",
+                  connection="STORAGE_CONNECTION_STRING")
+def test_function(zipfile: func.InputStream):
+    logger.info(f"Python blob trigger function processed blob {zipfile.name}")
+    try:
+        process(zipfile.name)
+    except BaseException as err:
+        logger.exception(err)
+
+def process(zipfile: str):
+    app_ver = get_version()
+    logger.info("version {}".format(app_ver))
+    logger.info(f"File: {zipfile}")
+    name = os.path.basename(zipfile)
+    company = os.path.splitext(name)[0]
+    logger.info(f"Company: {company}")
+    cfg = FynctionCfg()
+    logger.info(f"Cluster: {cfg.compute_cluster}")
+    input_file = cfg.input_path_template.format(company)
+    logger.info(f"Input file: {input_file}")
+    logger.info(f"Workspace: {cfg.workspace}")
+    logger.info(f"Subscription ID: {cfg.subscription_id}")
+    logger.info(f"ML component: {cfg.ml_component}")
+
+    # authenticate
+    credential = DefaultAzureCredential()
+    credential.get_token("https://management.azure.com/.default")
+
+    # Get a handle to the workspace
+    ml_client = MLClient(
+        credential=credential,
+        subscription_id=cfg.subscription_id,
+        resource_group_name="DocuBank",
+        workspace_name=cfg.workspace,
+    )
+
+    tune_component = ml_client.components.get(cfg.ml_component)
+    logger.info(f'output: {tune_component.outputs}')
+
+    @dsl.pipeline(compute=cfg.compute_cluster, description="Tune pipeline for {company}")
+    def pipeline(company, data):
+        output_path = cfg.output_path
+        tune_job = tune_component(company=company, data=data)
+        tune_job.outputs["output_path"] = Output(type="uri_folder", mode="rw_mount", path=output_path)
+        logger.info(f'output: {tune_job.outputs["output_path"]}')
+        return {
+            "output_path": tune_job.outputs.output_path
+        }
+
+    pl = pipeline(company=company, data=Input(type="uri_file", path=input_file))
+    pipeline_job = ml_client.jobs.create_or_update(pl, experiment_name=f"tune params for {company}")
+    logger.info(f'output: {pipeline_job.name}')
+    return pipeline_job.name
