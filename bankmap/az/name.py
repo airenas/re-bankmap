862b4ca842e941c22df16cbe65e322f9f9070e8f
Airenas Vaiciunas <airenass@gmail.com>
2023-12-12 15:49:20 +0200

    Fix company name as file name

--- bankmap/az/name.py
+++ bankmap/az/name.py
@@ -1,2 +1,18 @@
+
+def is_latin(letter):
+    if letter.isalpha():
+        return 65 <= ord(letter) <= 90 or 97 <= ord(letter) <= 122
+    else:
+        return False
+
+
+def replace_char(x):
+    if is_latin(x):
+        return x
+    if x in '-0123456789':
+        return x
+    return "_"
+
+
 def fix_exp_name(name: str, max_len: int = 50) -> str:
-    return ''.join(x for x in name if x.isalpha() or x in ' _-0123456789')[:max_len]
+    return ''.join(replace_char(x) for x in name)[:max_len]
--- bankmap/az/test_name.py
+++ bankmap/az/test_name.py
@@ -3,7 +3,8 @@ from bankmap.az.name import fix_exp_name
 
 def test_fix_exp_name():
     assert fix_exp_name('10') == '10'
-    assert fix_exp_name("map for asdsad") == "map for asdsad"
-    assert fix_exp_name("map for asdsad 12321") == "map for asdsad 12321"
-    assert fix_exp_name("map for asdsad-12321.,;}[)(") == "map for asdsad-12321"
-    assert fix_exp_name("map for asdsad-12321.,;}[)(", max_len=5) == "map f"
+    assert fix_exp_name("map for asdsad") == "map_for_asdsad"
+    assert fix_exp_name("map for asdsad 12321") == "map_for_asdsad_12321"
+    assert fix_exp_name("map for asdsad-12321.,;}[)(") == "map_for_asdsad-12321_______"
+    assert fix_exp_name("map for asdsad-12321.,;}[)(", max_len=5) == "map_f"
+    assert fix_exp_name("map for Čįėž a", max_len=15) == "map_for______a"
