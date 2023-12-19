e4b0adf586370c2e469d862e8c75c4d9afb7ef95
Airenas Vaiciunas <airenass@gmail.com>
2023-12-19 09:14:13 +0200

    Fix customer load

--- bankmap/loaders/ledgers.py
+++ bankmap/loaders/ledgers.py
@@ -34,10 +34,10 @@ def load_customer_sfs(ledgers_file_name, ba_file_name, cust_file_name):
     logger.info("loaded ledgers {} rows".format(len(ledgers)))
     accounts = pd.read_csv(ba_file_name, sep=',')
     logger.info("loaded ba {} rows".format(len(accounts)))
-    names = pd.read_csv(cust_file_name, sep=',')
+    names = pd.read_csv(cust_file_name, sep=',', dtype={'No_': str})
     logger.info("loaded sfs {} rows".format(len(names)))
 
-    c_d = {r['No_']: (r['Name'], MapType.from_s(e_str(r['Application_Method']))) for r in names.to_dict('records')}
+    c_d = {e_str(r['No_']): (r['Name'], MapType.from_s(e_str(r['Application_Method']))) for r in names.to_dict('records')}
     accounts_d = {r['Customer_No_']: r['Bank_Account_No_'] for r in accounts.to_dict('records')}
 
     return prepare_cust_sfs(ledgers, c_d, accounts_d)
@@ -73,7 +73,7 @@ def load_vendor_sfs(ledgers_file_name, ba_file_name, vend_file_name):
     logger.info("loaded ledgers {} rows".format(len(ledgers)))
     accounts = pd.read_csv(ba_file_name, sep=',')
     logger.info("loaded ba {} rows".format(len(accounts)))
-    names = pd.read_csv(vend_file_name, sep=',')
+    names = pd.read_csv(vend_file_name, sep=',', dtype={'No_': str})
     logger.info("loaded sfs {} rows".format(len(names)))
 
     c_d = {e_str(r['No_']): (r['Name'], MapType.from_s(e_str(r['Application_Method']))) for r in
