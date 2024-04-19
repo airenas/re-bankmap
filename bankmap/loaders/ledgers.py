0e2a9bfaa871a30cd473bf2e207ee3896dd6f5fa
Airenas Vaiciunas <airenass@gmail.com>
2024-04-19 12:34:12 +0300

    feat: ignore gls with empty number

--- bankmap/loaders/ledgers.py
+++ bankmap/loaders/ledgers.py
@@ -1,7 +1,6 @@
-import pandas as pd
 from jsonlines import jsonlines
 
-from bankmap.data import e_str, e_date, e_currency, e_float, MapType, DocType, LType, e_str_ne, e_str_first
+from bankmap.data import e_str, e_date, e_currency, e_float, MapType, DocType, LType, e_str_ne, e_str_first, e_str_e
 from bankmap.logger import logger
 
 
@@ -52,7 +51,7 @@ def load_customer_sfs(ledgers_file_name, ba_file_name, cust_file_name):
             res.append({'type': LType.CUST.to_s(), "number": _id, 'name': cd[0],
                         'iban': ibans.get(_id, ''), 'documentNumber': d['documentNumber'],
                         'dueDate': e_date(d, 'dueDate'),
-                        'documentDate': e_date(d,'documentDate'),
+                        'documentDate': e_date(d, 'documentDate'),
                         'externalDocumentNumber': d['externalDocumentNumber'],
                         'amount': e_float(d, 'amount'),
                         'currencyCode': e_currency(d.get('currencyCode')),
@@ -83,8 +82,8 @@ def load_vendor_sfs(ledgers_file_name, ba_file_name, vend_file_name):
             cd = names[_id]
             res.append({'type': LType.VEND.to_s(), "number": _id, 'name': cd[0],
                         'iban': ibans.get(_id, ''), 'documentNumber': d['documentNumber'],
-                        'dueDate': e_date(d,'dueDate'),
-                        'documentDate': e_date(d,'documentDate'),
+                        'dueDate': e_date(d, 'dueDate'),
+                        'documentDate': e_date(d, 'documentDate'),
                         'externalDocumentNumber': d['externalDocumentNumber'],
                         'amount': e_float(d, 'amount'),
                         'currencyCode': e_currency(d.get('currencyCode')),
@@ -102,26 +101,29 @@ def load_vendor_sfs(ledgers_file_name, ba_file_name, vend_file_name):
 # returns dataframe
 def load_gls(ledgers_file_name):
     logger.info("loading {}".format(ledgers_file_name))
-    res = []
+    res, skip = [], 0
     with jsonlines.open(ledgers_file_name) as reader:
         for (i, d) in enumerate(reader):
             if i == 0:
                 logger.debug(f"Item: {d}")
-            _id = e_str_ne(d, 'number')
-            res.append({'type': LType.GL.to_s(), "number": _id,
-                        'name': e_str_first(d, ['searchName', 'name']),
-                        'iban': '', 'documentNumber': '',
-                        'dueDate': None,
-                        'documentDate': None,
-                        'externalDocumentNumber': '',
-                        'amount': 0,
-                        'currencyCode': 'EUR',
-                        'documentType': LType.GL.to_s(),
-                        'closedAtDate': None,
-                        'mapType': MapType.UNUSED.to_s(),
-                        'open': True,
-                        'remainingAmount': 0})
-    logger.info(f"loaded {len(res)} rows in {ledgers_file_name}")
+            _id = e_str_e(d, 'number')
+            if _id:
+                res.append({'type': LType.GL.to_s(), "number": _id,
+                            'name': e_str_first(d, ['searchName', 'name']),
+                            'iban': '', 'documentNumber': '',
+                            'dueDate': None,
+                            'documentDate': None,
+                            'externalDocumentNumber': '',
+                            'amount': 0,
+                            'currencyCode': 'EUR',
+                            'documentType': LType.GL.to_s(),
+                            'closedAtDate': None,
+                            'mapType': MapType.UNUSED.to_s(),
+                            'open': True,
+                            'remainingAmount': 0})
+            else:
+                skip += 1
+    logger.info(f"loaded {len(res)} (skipped: {skip}) rows in {ledgers_file_name}")
     return res
 
 
